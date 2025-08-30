e5b7c54b8d5e7511026d5c3fd108a4a26ca86121
Pacman0126 <oliver.p.hartmann@gmail.com>
2025-08-30 16:33:56 +0200

    Add secret key

--- gambinos/settings.py
+++ gambinos/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-32g3-q4r+%iurg4o8_$r@hv*943#ir#@0ip22bn*84kj1b3phq'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
