4defae09a56c38ba73e9848136968b17a747bfb6
Pacman0126 <oliver.p.hartmann@gmail.com>
2025-09-01 16:49:46 +0200

    Debug urls

--- reservation_book/urls.py
+++ reservation_book/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReservationDisplay.as_view(), name='home'),
+    path('reservation_book/',
+         views.reservation_detail, name='reservation_id'),
 ]
