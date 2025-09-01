d86b77924aaa6c6ad1c4156e801f22cd463fdda4
Pacman0126 <oliver.p.hartmann@gmail.com>
2025-09-01 16:49:46 +0200

    Debug urls

--- reservation_book/views.py
+++ reservation_book/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from django.views import generic
 from .models import ReservationBook
 # from django.http import HttpResponse
@@ -12,3 +13,27 @@ class ReservationDisplay(generic.ListView):
     # model = ReservationBook
     queryset = ReservationBook.objects.all()
     template_name = "reservation_book/index.html"
+
+
+def reservation_detail(request, reservation_id):
+    """
+    Display an individual :model:`reservation_book.ReservationBook`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`reservation_book.ReservationBook`.
+
+    **Template:**
+
+    :template:`reservation_book/index.html`
+    """
+
+    queryset = ReservationBook.objects.all()
+    post = get_object_or_404(queryset, reservation_id=reservation_id)
+
+    return render(
+        request,
+        "reservation_book/index.html",
+        {"post": post},
+    )
