2f12a21504640e51b627286f483e05797f7db36b
Pacman0126 <oliver.p.hartmann@gmail.com>
2025-09-03 17:32:17 +0200

    Resolve pathing issues between templates and views

--- reservation_book/urls.py
+++ reservation_book/urls.py
@@ -4,6 +4,6 @@ from .views import home, reservations
 
 urlpatterns = [
     path('', home, name='home'),
-    path('reservation_book/', reservations),
+    path('reservation_book/', reservations, name='reservations'),
     # views.reservation_detail, name='reservation_detail'),
 ]
